a5270af1ea8ec1411d383fe521112c4dc9bac715
Sebastian Pipping <sebastian@pipping.org>
2021-08-13 21:56:15 +0200

    Bump version to 6.1.0

--- git_delete_merged_branches/_metadata.py
+++ git_delete_merged_branches/_metadata.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2020 Sebastian Pipping <sebastian@pipping.org>
+# Copyright (C) 2020-2021 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v3 or later
 
 APP = 'git-delete-merged-branches'
 DESCRIPTION = 'Command-line tool to delete merged Git branches'
-VERSION = '6.0.5'
+VERSION = '6.1.0'

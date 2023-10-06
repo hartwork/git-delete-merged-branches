ebabfd1c3712d8227131eac8e6aaa9f207e1167f
Sebastian Pipping <sebastian@pipping.org>
2023-10-06 15:48:03 +0200

    Add support for Python 3.12

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Version Control',
         'Topic :: Software Development :: Version Control :: Git',
     ],
